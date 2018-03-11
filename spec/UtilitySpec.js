b7018df4dde63242d620af45b2d4c068f44e43f1
Chet Bortz <chet@bortz.co>
2018-03-11 19:02:28 -0400

    DRY up implementation of today date stub

--- spec/UtilitySpec.js
+++ spec/UtilitySpec.js
@@ -6,6 +6,12 @@ describe("Utility Examples", function() {
   });
 
   describe("ageInYears()", function() {
+    const nowStub = new Date("2018-03-11");
+
+    beforeEach(() => {
+      spyOn(Date, "now").and.returnValue(nowStub);
+    });
+
     it("returns the age from a birthday", function(){
       expect(ageInYears("1980-05-17")).toEqual(37);
     });
@@ -14,18 +20,19 @@ describe("Utility Examples", function() {
   describe("isOver21()", () => {
     const nowStub = new Date("2018-03-11");
 
-    it("returns true when the age is 21", () => {
+    beforeEach(() => {
       spyOn(Date, "now").and.returnValue(nowStub);
+    });
+
+    it("returns true when the age is 21", () => {
       expect(isOver21("1997-01-01")).toBe(true);
     });
 
     it("returns true when the age is over 21", () => {
-      spyOn(Date, "now").and.returnValue(nowStub);
       expect(isOver21("1992-01-01")).toBe(true);
     });
 
     it("returns false when the age is under 21", () => {
-      spyOn(Date, "now").and.returnValue(nowStub);
       expect(isOver21("2000-01-01")).toBe(false);
     });
   });

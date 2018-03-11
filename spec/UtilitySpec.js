504cffa1e14f693cc38942b7f356793e2ccfe9b8
Chet Bortz <chet@bortz.co>
2018-03-11 18:37:32 -0400

    Add `ageInYears()` utility function

--- spec/UtilitySpec.js
+++ spec/UtilitySpec.js
@@ -4,4 +4,10 @@ describe("Utility Examples", function() {
       expect(fullName("Billy", "Bob")).toEqual("Billy Bob");
     });
   });
+
+  describe("ageInYears()", function() {
+    it("returns the age from a birthday", function(){
+      expect(ageInYears("1980-05-17")).toEqual(37);
+    });
+  });
 });
--- src/Utility.js
+++ src/Utility.js
@@ -1,3 +1,11 @@
 function fullName(firstName, lastName) {
   return `${firstName} ${lastName}`;
 }
+
+function ageInYears(birthdayString) {
+  const birthday = new Date(birthdayString);
+  const ageInMilliseconds = Date.now() - birthday;
+  const millisecondsPerYear = 365.25 * 24 * 60 * 60 * 1000; // 31557600000
+
+  return Math.floor(ageInMilliseconds / millisecondsPerYear);
+}

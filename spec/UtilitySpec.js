e68c24ad2c66a22d2d4f9a8a34bbfc2d64ee3713
Chet Bortz <chet@bortz.co>
2018-03-11 18:55:15 -0400

    Stub today's date for `isOver21()`

--- spec/UtilitySpec.js
+++ spec/UtilitySpec.js
@@ -12,15 +12,20 @@ describe("Utility Examples", function() {
   });
 
   describe("isOver21()", () => {
+    const nowStub = new Date("2018-03-11");
+
     it("returns true when the age is 21", () => {
+      spyOn(Date, "now").and.returnValue(nowStub);
       expect(isOver21("1997-01-01")).toBe(true);
     });
 
     it("returns true when the age is over 21", () => {
+      spyOn(Date, "now").and.returnValue(nowStub);
       expect(isOver21("1992-01-01")).toBe(true);
     });
 
     it("returns false when the age is under 21", () => {
+      spyOn(Date, "now").and.returnValue(nowStub);
       expect(isOver21("2000-01-01")).toBe(false);
     });
   });

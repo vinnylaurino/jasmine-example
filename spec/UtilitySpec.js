9623a192b4f449b2b16733a658087f143ccafb1e
vinnylaurino <vinny.laurino@gmail.com>
2018-03-13 19:35:28 -0400

    stub today's date in isOver21 utility function

--- spec/UtilitySpec.js
+++ spec/UtilitySpec.js
@@ -14,13 +14,18 @@ describe("untility functions" , function(){
 
 
   describe("isOver21()", () => {
+    const nowStub = new Date("2018-03-13");
+
     it("returns true when the age is 21", () => {
+      spyOn(Date, "now").and.returnValue(nowStub);
       expect(isOver21("1997-01-01")).toBe(true);
     });
     it("returns true when the age is over 21", () => {
+      spyOn(Date, "now").and.returnValue(nowStub);
       expect(isOver21("1992-01-01")).toBe(true);
     });
     it("returns false when the age is under 21", () => {
+      spyOn(Date, "now").and.returnValue(nowStub);
       expect(isOver21("2000-01-01")).toBe(false);
     });
   });

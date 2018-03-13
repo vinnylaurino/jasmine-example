ed6d0fa9f32951ad74b405dbe49f609c274021c3
vinnylaurino <vinny.laurino@gmail.com>
2018-03-13 19:21:31 -0400

    added ageIsOver21

--- spec/UtilitySpec.js
+++ spec/UtilitySpec.js
@@ -11,4 +11,18 @@ describe("untility functions" , function(){
       expect(ageInYears("1980-05-17")).toEqual(37);
     });
   });
+
+
+  describe("isOver21()", () => {
+    it("returns true when the age is 21", () => {
+      expect(isOver21("1997-01-01")).toBe(true);
+    });
+    it("returns true when the age is over 21", () => {
+      expect(isOver21("1992-01-01")).toBe(true);
+    });
+    it("returns false when the age is under 21", () => {
+      expect(isOver21("2000-01-01")).toBe(false);
+    });
+  });
+
 });
--- src/Utility.js
+++ src/Utility.js
@@ -1,13 +1,15 @@
 function fullName(firstName , lastName){
   return `${firstName} ${lastName}`;
 }
-
 function ageInYears(birthdayString){
   const birthday = new Date(birthdayString);
   const ageInMilliseconds = Date.now() - birthday;
   const millisecondsPerYear = 31557600000;
 
   return Math.floor(ageInMilliseconds / millisecondsPerYear);
+}
 
+function isOver21(birthdayString) {
+  return ageInYears(birthdayString) >= 21;
 
 }
